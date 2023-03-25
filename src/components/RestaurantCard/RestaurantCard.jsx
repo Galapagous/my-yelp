d5c1488b7b96a404db3a317de0a7edf91e5437d4
Muhammed MUsa <72840992+Galapagous@users.noreply.github.com>
2023-03-25 03:54:49 +0100

    toggling delete functionality

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,15 +1,16 @@
 import "./restaurantCard.scss"
-import { API, graphqlOperation } from 'aws-amplify'
-import { deleteRestaurant } from "../../graphql/mutations"
+// import { API, graphqlOperation } from 'aws-amplify'
+// import { deleteRestaurant } from "../../graphql/mutations"
 
 const RestaurantCard = (props)=>{
-  const handleDelete = async()=>{
-    try {
-      await API.graphql(graphqlOperation(deleteRestaurant, {input: {id: props.info.id}}))
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  //---------Delete funcionality toggler----------"
+  // const handleDelete = async()=>{
+  //   try {
+  //     await API.graphql(graphqlOperation(deleteRestaurant, {input: {id: props.info.id}}))
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
   return(
     <div className="restaurant">
     <div className="res-container">
@@ -20,7 +21,7 @@ const RestaurantCard = (props)=>{
       <h4>{props.info.state}</h4>
       </div>
     </div>
-    <button onClick={handleDelete}>Delete</button>
+    {/* <button onClick={handleDelete}>Delete</button> */}
     </div>
   )
 }
